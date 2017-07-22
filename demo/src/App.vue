bc44f135995fe978775603cbb6f390b316ea1ba9
Ivan Kuznetsov <ivankuznetsov316@gmail.com>
2017-07-22 22:11:01 +0300

    demo grammar

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -15,7 +15,7 @@
 
       <div class="row">
         <div class="col-md-12">
-          <h1>Grouping images</h1>
+          <h1>Grouped images</h1>
           <div class="row">
             <div class="col-md-6">
               <h2>Group 1</h2>
@@ -44,7 +44,7 @@
 
       <div class="row">
         <div class="col-md-12">
-          <h1>Dynamically grouping images</h1>
+          <h1>Dynamically grouped images</h1>
           <div class="row">
             <div class="col-md-12">
               <div class="well gal">
