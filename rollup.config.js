b020fd23273880513e3d2226d508645eea9afde2
Maxim <acupofspirt@gmail.com>
2017-09-06 18:50:35 +0300

    Decrease bundle size

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,29 @@ import vue from 'rollup-plugin-vue'
 import babel from 'rollup-plugin-babel'
 import babelrc from 'babelrc-rollup'
 import istanbul from 'rollup-plugin-istanbul'
+import uglify from 'rollup-plugin-uglify'
+import {minify} from 'uglify-es'
 
 const pkg = require('./package.json')
 const external = Object.keys(pkg.dependencies)
+const plugins = [
+  vue({
+    css: true
+  }),
+  babel(babelrc())
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(uglify({}, minify))
+}
+else {
+  plugins.push(istanbul({
+    exclude: ['test/**/*', 'node_modules/**/*']
+  }))
+}
 
 export default {
   input: 'lib/index.js',
-  plugins: [
-    vue({
-      css: true
-    }),
-    babel(babelrc()),
-    istanbul({
-      exclude: ['test/**/*', 'node_modules/**/*']
-    })
-  ],
-  external,
   output: [
     {
       file: pkg.main,
@@ -30,5 +37,7 @@ export default {
       format: 'es',
       sourcemap: true
     }
-  ]
+  ],
+  plugins,
+  external
 }
