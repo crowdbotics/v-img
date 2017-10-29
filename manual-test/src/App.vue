7284f288dcee194b6683449f211b909e6b98ae07
Ivan Kuznetsov <ivankuznetsov316@gmail.com>
2017-10-29 15:00:26 +0300

    Manual test cleaned

--- manual-test/src/App.vue
+++ manual-test/src/App.vue
@@ -1,26 +1,11 @@
 <template>
   <div>
-    <input v-model="title" placeholder="title" type="text">
-    <input v-model="group" placeholder="group" type="text">
-    <input v-model="openOn" placeholder="openOn" type="text">
-    <img v-img:a="{ sourceButton: true, title, group, openOn }" src="https://unsplash.it/200/300?image=2">
-
-    <img v-img:v="{title: 'titlehere haha haha'}"
-      src="https://unsplash.it/200/300?image=1">
-
-    <img v-img:v
-      src="https://unsplash.it/200/300?image=3">
+    <img v-img:a="{ sourceButton: true, openOn: 'dblclick' }" src="https://unsplash.it/200/300?image=2">
+    <img v-img:v="{ title: 'Example title' }" src="https://unsplash.it/200/300?image=1">
+    <img v-img:v src="https://unsplash.it/200/300?image=3">
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      title: '',
-      group: '',
-      openOn: 'dblclick',
-    }
-  },
-};
+export default {};
 </script>
