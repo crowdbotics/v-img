97c8e8fd01c116d339a7ee3ea456e0e2d5c0bf54
Ivan Kuznetsov <ivankuznetsov316@gmail.com>
2017-10-31 12:37:21 +0300

    [0.1.9] Fix for #36

--- manual-test/src/App.vue
+++ manual-test/src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
+    <input type="number" v-model="n" id="">
     <img v-img src="https://unsplash.it/200/300?image=2">
     <img v-img src="https://unsplash.it/200/300?image=324">
-    <img v-img:v="{ title: 'Example title' }" src="https://unsplash.it/200/300?image=1">
+    <img v-img="{ src: `https://unsplash.it/200/300?image=${n}` }" :diabled="n === 2">
     <img v-img:v src="https://unsplash.it/200/300?image=3">
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      n: 1,
+    }
+  },
+};
 </script>
+
+<style>
+img {
+  width: 200px;
+  height: 100px;
+}
+</style>
+
