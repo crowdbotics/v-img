fcd48bfd97b9c6f15416907462e54f163251d9ae
heyitskippy <heyitskippy@gmail.com>
2018-03-27 00:14:37 +0300

    Added the name option (for download attribute)

--- manual-test/src/App.vue
+++ manual-test/src/App.vue
@@ -5,7 +5,7 @@
     <img v-img:v src="https://unsplash.it/200/300?image=324">
     <img v-img="{ src: `https://unsplash.it/200/300?image=${n}` }" :diabled="n === 2">
     <img v-img:v src="https://unsplash.it/200/300?image=3">
-    <span v-img="{ group: 'v', src: 'https://unsplash.it/200/300?image=10' }">test</span>
+    <span v-img="{ group: 'v', src: 'https://unsplash.it/200/300?image=10', sourceButton: true, name: 'something' }" style="cursor:pointer;">test</span>
   </div>
 </template>
 
